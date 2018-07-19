b0b0306abfc8d3cccd5fae96f583c1a26b745555
Andres <andreslago5@hotmail.com>
2018-07-19 14:52:05 -0400

    Update miditocsv.py

--- miditocsv.py
+++ miditocsv.py
@@ -2,13 +2,11 @@ import os, sys
 
 path="./midi/"
 dirs=os.listdir(path)
-print(dirs)
 dirs2=[]
 for file in dirs:
   if ".mid" in file:
     dirs2.append(file)
-print(dirs2)
-print(len(dirs2))
+
 os.system('mkdir csv')
 for a in dirs2:
   os.system('midicsv midi/'+a+ ' csv/'+os.path.splitext(a)[0]+'.csv')
