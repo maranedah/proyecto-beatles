34c21f1770e75cf450b8be9b678dcaad859d3f47
Andres <andreslago5@hotmail.com>
2018-08-11 13:18:20 -0400

    Listo para ejecutar todo el proceso solamente llamando a main

--- miditocsv.py
+++ miditocsv.py
@@ -7,8 +7,10 @@ for file in dirs:
   if ".mid" in file:
     dirs2.append(file)
 
-os.system('mkdir csv')
+os.makedirs('csv',exist_ok=True)
 for a in dirs2:
-  os.system('midicsv-1.1/midicsv midi/'+a+ ' csv/'+os.path.splitext(a)[0]+'.csv')
+  path_midi = os.path.join("midi",a)
+  path_csv_out = os.path.join("csv",os.path.splitext(a)[0]+'.csv')
+  os.system('midicsv-1.1/midicsv "{0}" "{1}" '.format(path_midi,path_csv_out))
 print("Terminado")
 
